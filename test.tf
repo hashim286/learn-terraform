acc8184184a528e1584e31e120f07394539efab5
hashim-rauf <hashimrauf@gmail.com>
2024-02-17 17:36:31 -0500

    Learned how to provide credentials without hardcoding, added it to the template

--- test.tf
+++ test.tf
@@ -9,16 +9,16 @@ terraform {
 }
 
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "KEY_ID"
-  secret_key = "KEY_VAL"
-
+  region                   = "us-east-1"
+  shared_config_files      = ["C:/Users/hrauf/.aws/config"]
+  shared_credentials_files = ["C:/Users/hrauf/.aws/credentials"]
+  profile                  = "iamadmin-general"
 }
 
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allows SSH from any source IP"
-  vpc_id      = "VPC_ID"
+  vpc_id      = "vpc-09bd74efc602cf216"
   tags = {
     Name = "allow_ssh"
   }
@@ -41,11 +41,11 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_ipv4" {
 }
 
 resource "aws_instance" "app_server" {
-  ami                    = "AMI_ID"
+  ami                    = "ami-0e731c8a588258d0d"
   instance_type          = "t2.micro"
-  key_name               = "KEY_NAME"
+  key_name               = "hr-test"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
   tags = {
-    Name = "NAME"
+    Name = "test"
   }
 }
