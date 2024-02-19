7834862e51f6e1853723834cc4cb0f792e050973
hashim286 <112742556+hashim286@users.noreply.github.com>
2024-02-19 18:19:04 -0500

    config files

---
+++ side_projects/ec2_website/config.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region                   = "us-east-1"
+  shared_config_files      = ["C:/Users/hashim/.aws/config"]
+  shared_credentials_files = ["C:/Users/hashim/.aws/credentials"]
+  profile                  = "iamadmin-general"
+}
