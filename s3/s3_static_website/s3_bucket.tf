5e08c33002ba0b3aa1f4395a0b3acbdb5e21c8e0
hashim-rauf <hashimrauf@gmail.com>
2024-02-18 23:06:01 -0500

    Fixed the static website thing finally, also removed unnecessary things from it

--- s3/s3_static_website/s3_bucket.tf
+++ s3/s3_static_website/s3_bucket.tf
@@ -1,67 +1,7 @@
-resource "aws_s3_bucket" "hr-test-bucket-2024-02-17" {
-  bucket = "hr-test-bucket-2024-02-17"
-
-  tags = {
-    Name        = "My Bucket"
-    Environment = "dev"
-  }
-}
-
-
-resource "aws_s3_bucket_website_configuration" "static_website" {
-  bucket = aws_s3_bucket.hr-test-bucket-2024-02-17.id
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-}
-
-
-
-resource "aws_s3_bucket_ownership_controls" "test" {
-  bucket = aws_s3_bucket.hr-test-bucket-2024-02-17.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "allow_public_read" {
-  bucket                  = aws_s3_bucket.hr-test-bucket-2024-02-17.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-
-resource "aws_s3_bucket_acl" "test" {
-  depends_on = [aws_s3_bucket_public_access_block.allow_public_read, aws_s3_bucket_ownership_controls.test]
-  bucket     = aws_s3_bucket.hr-test-bucket-2024-02-17.id
-  acl        = "public-read"
-}
-
-resource "aws_s3_object" "index" {
-  key          = "index.html"
-  bucket       = aws_s3_bucket.hr-test-bucket-2024-02-17.id
-  source       = "index.html"
-  content_type = "text/html"
-}
-
-resource "aws_s3_object" "error" {
-  key          = "error.html"
-  bucket       = aws_s3_bucket.hr-test-bucket-2024-02-17.id
-  source       = "error.html"
-  content_type = "text/html"
-
-}
-
-data "aws_iam_policy_document" "example" {
+data "aws_iam_policy_document" "example" { # define IAM policy to permit public read
   statement {
-    sid     = "1"
-    actions = ["s3:*"]
+    sid     = "PublicReadGetObject"
+    actions = ["s3:GetObject"]
     resources = [
       "${aws_s3_bucket.hr-test-bucket-2024-02-17.arn}/*",
     ]
@@ -72,7 +12,51 @@ data "aws_iam_policy_document" "example" {
   }
 }
 
-resource "aws_s3_bucket_policy" "example" {
+resource "aws_s3_bucket_policy" "example" { # attaching policy made above to bucket, use depends on to tell terraform that this requires public access to be granted before this can be attached to bucket
   bucket = aws_s3_bucket.hr-test-bucket-2024-02-17.id
   policy = data.aws_iam_policy_document.example.json
+  depends_on = [ aws_s3_bucket_public_access_block.allow_public_read ]
 }
+
+resource "aws_s3_bucket" "hr-test-bucket-2024-02-17" { # creates the bucket
+  bucket = "hr-bucket-123"
+
+  tags = {
+    Name        = "My Bucket"
+    Environment = "dev"
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "static_website" { # defines static website config details
+  bucket = aws_s3_bucket.hr-test-bucket-2024-02-17.id
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "allow_public_read" { # turns public access on, stops AWS from blocking public policies
+  bucket                  = aws_s3_bucket.hr-test-bucket-2024-02-17.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_object" "index" { # uploads object to s3
+  key          = "index.html"
+  bucket       = aws_s3_bucket.hr-test-bucket-2024-02-17.id
+  source       = "index.html"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "error" { # uploads object to s3
+  key          = "error.html"
+  bucket       = aws_s3_bucket.hr-test-bucket-2024-02-17.id
+  source       = "error.html"
+  content_type = "text/html"
+}
+
