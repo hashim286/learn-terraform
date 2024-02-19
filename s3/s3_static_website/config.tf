efc06ad46ee7b45607c21864a29437d912a30f99
hashim-rauf <hashimrauf@gmail.com>
2024-02-18 23:05:22 -0500

    config file

--- s3/s3_static_website/config.tf
+++ s3/s3_static_website/config.tf
@@ -11,6 +11,6 @@ terraform {
 provider "aws" {
   region                   = "us-east-1"
   shared_config_files      = ["C:/Users/hashim/.aws/config"]
-  shared_credentials_files = ["C:/Users/hashim/.aws/credentials"]
+  shared_credentials_files = ["C:/Users//.aws/credentials"]
   profile                  = "iamadmin-general"
 }
