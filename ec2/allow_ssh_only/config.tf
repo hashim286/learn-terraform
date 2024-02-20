458e38d3b1c51fea4e7781e2ee7848f7f5705b3b
hashim286 <112742556+hashim286@users.noreply.github.com>
2024-02-20 11:05:03 -0500

    config files, added tfc info

--- ec2/allow_ssh_only/config.tf
+++ ec2/allow_ssh_only/config.tf
@@ -8,8 +8,17 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+terraform {
+  cloud {
+    workspaces {
+      name = "learn-tfc-aws"
+
+    }
+    organization = "hr-home"
+  }
+}
+
 provider "aws" {
-  region                   = "us-east-1"
   shared_config_files      = ["C:/Users/hashim/.aws/config"]
   shared_credentials_files = ["C:/Users/hashim/.aws/credentials"]
   profile                  = "iamadmin-general"
--- s3/s3_static_website/config.tf
+++ s3/s3_static_website/config.tf
@@ -8,8 +8,18 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+terraform {
+  cloud {
+    organization = "hr-home"
+
+    workspaces {
+      name = "learn-tfc-aws"
+    }
+  }
+}
+
+
 provider "aws" {
-  region                   = "us-east-1"
   shared_config_files      = ["C:/Users/hashim/.aws/config"]
   shared_credentials_files = ["C:/Users/hashim/.aws/credentials"]
   profile                  = "iamadmin-general"
